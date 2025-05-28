d3b005aff1eecc46594785a78bed6f98da569aea
d3sti <83227941+d3stiii@users.noreply.github.com>
2025-05-28 19:48:10 +0200

    final fix!!

---
+++ backend-restoran/Migrations/20250528174600_RemoveDishDescription.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend_restoran.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDishDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Dishes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Dishes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- backend-restoran/Persistence/Models/Dish.cs
+++ backend-restoran/Persistence/Models/Dish.cs
@@ -5,7 +5,6 @@ namespace backend_restoran.Persistence.Models;
 public class Dish : BaseEntity
 {
   public string Title { get; set; }
-  public string Description { get; set; }
   public string PhotoUrl { get; set; }
   public decimal Price { get; set; }
   public decimal Weight { get; set; }
