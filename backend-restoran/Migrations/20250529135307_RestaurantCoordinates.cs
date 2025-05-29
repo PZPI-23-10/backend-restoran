8fbc118fc0fae39051e24386d79b6abebde5e324
d3sti <83227941+d3stiii@users.noreply.github.com>
2025-05-29 15:53:26 +0200

    migration

---
+++ backend-restoran/Migrations/20250529135307_RestaurantCoordinates.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend_restoran.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestaurantCoordinates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Latitude",
+                table: "Restaurants",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Longitude",
+                table: "Restaurants",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Restaurants");
+        }
+    }
+}
