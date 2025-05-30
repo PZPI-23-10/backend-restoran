df458206eeef48182387dd863279d67c7480d83e
d3sti <83227941+d3stiii@users.noreply.github.com>
2025-05-30 19:06:35 +0200

    Update RestaurantCuisine.cs

--- backend-restoran/Persistence/Models/RestaurantCuisine.cs
+++ backend-restoran/Persistence/Models/RestaurantCuisine.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace backend_restoran.Persistence.Models;
 
@@ -8,5 +9,5 @@ public class RestaurantCuisine : BaseEntity
   public Restaurant Restaurant;
 
   [ForeignKey(nameof(Cuisine))] public Guid CuisineId { get; set; }
-  public Cuisine Cuisine;
+  [JsonInclude] public Cuisine Cuisine;
 }
