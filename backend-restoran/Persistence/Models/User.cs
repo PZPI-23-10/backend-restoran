24fa92f684fdbe01337e9f6272fc4e207645b854
d3sti <83227941+d3stiii@users.noreply.github.com>
2025-06-01 21:33:16 +0200

    Update User.cs

--- backend-restoran/Persistence/Models/User.cs
+++ backend-restoran/Persistence/Models/User.cs
@@ -3,7 +3,7 @@
 public class User : BaseEntity
 {
   public string FirstName { get; set; }
-  public string LastName { get; set; }
+  public string? LastName { get; set; }
   public string? MiddleName { get; set; }
   public string Email { get; set; }
   public string? Password { get; set; }
