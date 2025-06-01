852e3c3d9822daf087f6ce7e08cc3dbe0e11677b
d3sti <83227941+d3stiii@users.noreply.github.com>
2025-06-01 18:38:50 +0200

    Update 20250601155826_GoogleAuth.cs

--- backend-restoran/Migrations/20250601155826_GoogleAuth.cs
+++ backend-restoran/Migrations/20250601155826_GoogleAuth.cs
@@ -1,3 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend_restoran.Migrations
+{
+    /// <inheritdoc />
+    public partial class GoogleAuth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
             migrationBuilder.AddColumn<bool>(
                 name: "IsGoogleAuth",
                 table: "Users",
